742be5c11a83330ee50ab7cc5027e9c5fa63898e
Sumit Kumar <sumit.sh25@yahoo.com>
2016-08-22 21:25:38 +0530

    Update form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -21,7 +21,7 @@ if(IsInjected($visitor_email))
     exit;
 }
 
-$email_from = 'tom@amazing-designs.com';//<== update the email address
+$email_from = 'sumit.ksh25@gmail.com';//<== update the email address
 $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message".
